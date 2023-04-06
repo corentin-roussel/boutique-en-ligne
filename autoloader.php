945607d29b27e879ff361e3740cefccae7901913
corentin-roussel <corentin.roussel@laplateforme.io>
2023-04-06 17:06:21 +0200

    DEV [+] 17:04 repare signin/signup

--- autoloader.php
+++ autoloader.php
@@ -4,14 +4,12 @@
 
 function my_autoloader($class)
 {
-    var_dump($class);
-    $class_path = str_replace("\\", "/", $class);
-    var_dump($class_path);
-    $class_path = str_replace("App", "src", $class_path);
 
+    $class_path = str_replace("\\", "/", $class);
+    $class_path = str_replace("App", "src", $class_path);
     $file = __DIR__ . '/' . $class_path . ".php";
 
-    var_dump($file);
+
     if(file_exists($file))
     {
         require_once $file;
